ff5383139177897520bbaf16089519c0b1b46a4e
DionSalihu123 <dionsalihu14@gmail.com>
2026-05-18 12:11:37 +0200

    fixed(rescourse-service-1): u ndreq versioni i JWT token ne perputhje me .NET 9

--- resource-service-1/Program.cs
+++ resource-service-1/Program.cs
@@ -1,8 +1,32 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!)
+            )
+        };
+    });
+
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -11,6 +35,9 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.UseHttpsRedirection();
 
--- resource-service-1/obj/Debug/net9.0/resource-service-1.AssemblyInfo.cs
+++ resource-service-1/obj/Debug/net9.0/resource-service-1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("resource-service-1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a531524e3cbc567a2b016a1a81bcec471c087c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("resource-service-1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("resource-service-1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
