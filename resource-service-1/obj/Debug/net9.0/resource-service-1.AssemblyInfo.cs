9c7a68e942317f8c614f0ad8d1be6cd2e120ef82
DionSalihu123 <dionsalihu14@gmail.com>
2026-05-18 12:32:59 +0200

    o shtu ni gateaway i thjesht + yarp

--- resource-service-1/obj/Debug/net9.0/resource-service-1.AssemblyInfo.cs
+++ resource-service-1/obj/Debug/net9.0/resource-service-1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("resource-service-1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff5383139177897520bbaf16089519c0b1b46a4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0af29724b8b8e305826142a0ed17731d5a82c964")]
 [assembly: System.Reflection.AssemblyProductAttribute("resource-service-1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("resource-service-1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
